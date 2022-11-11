3f3468bb5ff4357e09f1492b0b4ce2ffe3f6ea6a
Bryan Silva <bryan@leadboxhq.com>
2022-11-10 23:40:26 -0300

    New Style Tweaks

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,3 +1,7 @@
-<div class="text-4xl font-bold text-center text-blue-400 page-header">
-  <h1>{!! $title !!}</h1>
+<div class="flex flex-col justify-center w-full py-8 h-screen page-header bg-[#F2F2F2]">
+  <a href="/" class="text-4xl font-bold text-center text-black uppercase">{{ _e( 'Sage Development Tech', 'sagedevelopment' ) }}</a>
+  <div class="w-full content">
+    <h2 class="py-1 text-center">{{ _e('Sitio en construcción para compartir información acerca de las herramientas de', 'sagedevelopment') }} <a class="font-bold text-blue-500 transition-all hover:text-blue-300" href="https://roots.io/">Roots.</a></h2>
+    <p class="text-center">{{ _e( 'Hecho con ❤ por', 'sagedevelopment') }} <a class="font-bold text-gray-600 transition-all hover:text-gray-500" href="https://bryandev.tech/">Bryan Silva.</a></p>
+  </div>
 </div>
