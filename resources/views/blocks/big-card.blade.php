124d61f3efee1c10d0fb0a3ff87e7be7da91b91f
Bryan Silva <bryan@leadboxhq.com>
2023-02-05 15:30:12 -0300

    Style Tweaks

--- resources/views/blocks/big-card.blade.php
+++ resources/views/blocks/big-card.blade.php
@@ -14,7 +14,7 @@
         </div>
       </a>
       {{-- Button --}}
-      <a class="text-center font-jost text-lg text-white hover:text-primary flex items-center justify-center gap-3 transition-all w-1/3 bg-secondary rounded-[18px] p-7" href="{{ $link }}" target="_blank">
+      <a class="text-center mb-12 xl:mb-0 font-jost text-lg text-white hover:text-primary flex items-center justify-center gap-3 transition-all w-full xl:w-1/3 bg-secondary rounded-[18px] p-7" href="{{ $link }}" target="_blank">
         {{ $link_text }}
         <svg width="19" height="17" viewBox="0 0 19 17" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M2 8.43542L15.7232 8.29857M10.6182 1.91138L17.1412 8.43436L10.4868 15.0887" stroke="#CDA274" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"/>
--- resources/views/blocks/brands.blade.php
+++ resources/views/blocks/brands.blade.php
@@ -1,5 +1,5 @@
 <div class="{{ $block->classes }}">
-  <div class="reviews-block flex space-x-28 items-center">
+  <div class="reviews-block flex flex-col lg:flex-row space-y-8 xl:space-y-0 xl:space-x-28 items-center">
     @foreach($brands as $brand)
       <img class="object-contain w-full max-h-28" src="{{ $brand['image'] }}" alt="{{ $brand['name'] }} Logo Image">
     @endforeach
