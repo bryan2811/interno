398b412afef981b95f2d7c2cba56ee4b68ad262c
Bryan Silva <bryan@leadboxhq.com>
2023-02-06 09:15:59 -0300

    Add documentation

--- app/View/Components/Navigation.php
+++ app/View/Components/Navigation.php
@@ -31,6 +31,11 @@ class Navigation extends Component
     /**
      * Create a new component instance.
      *
+     * The function takes three arguments, assigns them to three properties, and then does nothing else
+     * @param menuName The name of the menu you want to display.
+     * @param themeLocation This is the name of the theme location that you want to display.
+     * @param menuClass The class that will be applied to the ul element which forms the menu.
+     *
      * @return void
      */
     public function __construct($menuName = '', $themeLocation = '', $menuClass = '')
